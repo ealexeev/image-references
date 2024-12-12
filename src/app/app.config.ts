97b373d1f9e5edee060aa3f144cf0e94d3ed5c2f
Eugene Alexeev <eugene.alexeev@gmail.com>
2024-12-11 20:12:47 -0800

    New start with Angular 18, Angular/Material,and Angular/Fire.  May it actually work!

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"***REDACTED***","appId":"***REDACTED APP ID***","storageBucket":"***REDACTED***.firebasestorage.app","apiKey":"***REDACTED API KEY***","authDomain":"***REDACTED***.firebaseapp.com","messagingSenderId":"***REDACTED SENDER ID***"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())]
 };

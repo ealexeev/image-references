533f72dcea80fa1f40c7aa3071ca406a0d8cfa47
Eugene Alexeev <eugene.alexeev@gmail.com>
2024-12-12 21:50:49 -0800

    Got username + password auth to work.  Its ugly but it works!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, Signal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Auth, authState, User, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Subscription } from 'rxjs';
+
+// Don't show UI to users with a UID other than this.  Storage and Firebase APIs are also gated by this requirement.
+const permittedUid = "***REDACTED UID***";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+  private auth = inject(Auth);
+  authState$ = authState(this.auth);
+  authStateSubscription: Subscription;
+  user: User|null = null;
+  username = '';
+  password = '';
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  constructor() {
+    this.authStateSubscription = this.authState$.subscribe(( aUser: User | null) => {
+      this.user = aUser;
+    });
+  }
+
+  ngOnDestroy() {
+    this.authStateSubscription.unsubscribe();
+  }
+
+  getUserInfo(): string {
+    return JSON.stringify(this.user || {});
+  }
+
+  doLogin() {
+    signInWithEmailAndPassword(this.auth, this.username, this.password);
+  }
+
   title = 'prestige-ape-references';
 }

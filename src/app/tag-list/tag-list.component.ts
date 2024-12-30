1073028b1a938879ae4931c6ddc950e1fceeb7e7
Eugene Alexeev <eugene.alexeev@gmail.com>
2024-12-29 21:47:27 -0800

    Got a basic version of search going for tags!

--- src/app/tag-list/tag-list.component.ts
+++ src/app/tag-list/tag-list.component.ts
@@ -1,22 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { LiveTag, StorageService } from '../storage.service';
-import { Observable } from 'rxjs';
+import { combineLatestWith, debounceTime, defaultIfEmpty, distinctUntilChanged, map, startWith, tap, Observable } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
 import {MatDividerModule} from '@angular/material/divider';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
-
+import {MatInputModule} from '@angular/material/input';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-tag-list',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
     MatChipsModule,
     MatDividerModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './tag-list.component.html',
   styleUrl: './tag-list.component.scss'
@@ -24,8 +30,24 @@ import {MatListModule} from '@angular/material/list';
 export class TagListComponent implements OnInit{
   tags$: Observable<LiveTag[]>;
 
+  readonly searchText = new FormControl('')
+
   constructor(private storage: StorageService) {
-    this.tags$ = this.storage.tags$.asObservable();
+    var search = this.searchText.valueChanges.pipe(
+      startWith(''),
+      takeUntilDestroyed(),
+      debounceTime(250),
+    );
+    this.tags$ = this.storage.tags$.pipe(
+      // distinctUntilChanged(),
+      combineLatestWith(search),
+      tap( ([tags, searchText]) => {
+        console.log(`Search: ${searchText}, tags: ${tags}`)
+      }),
+      map( ([tags, searchText]) => {
+        return tags.filter(t => t.name.toLowerCase().includes((searchText || '').toLowerCase()))
+      }),
+    );
   }
 
   ngOnInit() {

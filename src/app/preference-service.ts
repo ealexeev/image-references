f1518b023dc04836f1284a2890769225b834d48d
Eugene Alexeev <eugene.alexeev@gmail.com>
2025-02-04 11:15:25 -0800

    Added updates to localStorage and initialization from locaStorage.
    Still need to wire up the UI elements to initialize their state from the preference service so that they indicate the actual behavior rather than local defaults.

--- src/app/preference-service.ts
+++ src/app/preference-service.ts
@@ -1,11 +1,47 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, debounceTime, distinctUntilChanged} from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreferenceService {
+  private readonly defaultAllImages: boolean = true;
+  private readonly defaultImageCount: number = 10;
+  private readonly localStoragePrefix: string = 'pa-puh-reference-';
+  private readonly showAllStorageKey: string = 'showAllImages'
+  private readonly imageCountStorageKey: string = 'imageCount';
+  showAllImages$: BehaviorSubject<boolean>;
+  showImageCount$: BehaviorSubject<number>;
 
-  showAllImages$  = new BehaviorSubject(true);
-  showImageCount$ = new BehaviorSubject(10);
+  constructor() {
+    const storedShowAll = localStorage.getItem(this.localStoragePrefix+this.showAllStorageKey);
+    if ( storedShowAll ) {
+      this.showAllImages$ = new BehaviorSubject(JSON.parse(storedShowAll));
+    } else {
+      this.showAllImages$ = new BehaviorSubject(this.defaultAllImages);
+    }
+
+    const storedImageCount = localStorage.getItem(this.localStoragePrefix+this.imageCountStorageKey);
+    if ( storedImageCount ) {
+      this.showImageCount$ = new BehaviorSubject(JSON.parse(storedImageCount));
+    } else {
+      this.showImageCount$ = new BehaviorSubject(this.defaultImageCount);
+    }
+
+    this.showAllImages$.pipe(
+      takeUntilDestroyed(),
+      debounceTime(5000),
+      distinctUntilChanged(),
+    ).subscribe(
+      (v: boolean) => {localStorage.setItem(this.localStoragePrefix+this.showAllStorageKey, JSON.stringify(v));},
+    )
+    this.showImageCount$.pipe(
+      takeUntilDestroyed(),
+      debounceTime(5000),
+      distinctUntilChanged(),
+    ).subscribe(
+      (v: number) => {localStorage.setItem(this.localStoragePrefix+this.imageCountStorageKey, JSON.stringify(v));},
+    )
+  }
 }

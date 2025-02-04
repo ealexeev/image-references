31cf5b1301c618a446979b36b66f1578cd85c08e
Eugene Alexeev <eugene.alexeev@gmail.com>
2025-02-04 10:47:31 -0800

    Fixed /tags route -- correctly hands missing parameter.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { TagMainComponent } from './tag-main/tag-main.component';
+import {TagSelectComponent} from './tag-select/tag-select.component';
 
 export const routes: Routes = [
 	{ path: 'tags/:tagName', component: TagMainComponent },
+  { path: 'tags', component: TagMainComponent },
+  { path: 'select', component: TagSelectComponent },
 ];

688372e311363ee32ba124b793390b47eb389d5a
Eugene Alexeev <eugene.alexeev@gmail.com>
2025-02-16 21:01:44 -0800

    Created TagService with complete unit test coverage to break up StorageService.
    There is still a slight coupling in that ImageService will need to update TagService about recently applied tags since those additions are applied to Image objects.  But this should break up and simplify component testing.
    
    TagService comes with a FakeTagService which will make things even easier to work with.

---
+++ src/app/tag.service.spec.ts
@@ -0,0 +1,111 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TagService } from './tag.service';
+import {signal} from '@angular/core';
+import {FirebaseApp, initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {environment} from './environments/environment.dev';
+import {collection, Firestore, getDocs, provideFirestore, query, writeBatch} from '@angular/fire/firestore';
+import {EmulatedFirestore} from './test-providers';
+import {EncryptionService} from './encryption.service';
+import {firstValueFrom, Subject, takeUntil} from 'rxjs';
+
+describe('TagService', () => {
+  let service: TagService;
+  const connected = signal(false);
+  let firestore: Firestore;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideFirebaseApp(() => initializeApp(environment)),
+        provideFirestore(()=> EmulatedFirestore(connected)),
+        {provide: EncryptionService},
+      ]
+    }).compileComponents()
+
+    service = TestBed.inject(TagService);
+    TestBed.inject(FirebaseApp)
+    TestBed.inject(Firestore)
+    firestore = service['firestore'];
+  });
+
+  afterEach(async () => {
+    const q = query(collection(firestore, 'tags'))
+    const docs = await getDocs(q);
+    const batch = writeBatch(firestore)
+    docs.forEach(doc => {batch.delete(doc.ref);});
+    await batch.commit();
+  })
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should store a tag', async () => {
+    const tagName = 'test-tag';
+    const created = await service.StoreTag(tagName)
+    expect(created).toBeTruthy();
+    expect(created.name).toEqual(tagName);
+  })
+
+  it('should load a tag by name', async () => {
+    const tagName = 'test-tag';
+    const created = await service.StoreTag(tagName)
+    const loaded = await service.LoadTagByName(tagName);
+    expect(loaded).toBeTruthy();
+    expect(loaded.name).toEqual(tagName);
+    expect(loaded.reference.id).toEqual(created.reference.id);
+  })
+
+  it('should load a tag by reference', async () => {
+    const tagName = 'test-tag';
+    const created = await service.StoreTag(tagName)
+    const loaded = await service.LoadTagByReference(created.reference);
+    expect(loaded).toBeTruthy();
+    expect(loaded.name).toEqual(tagName);
+    expect(loaded.reference.id).toEqual(created.reference.id);
+  })
+
+  it('storing tags should increment count', async () => {
+    const initial = await firstValueFrom(service.tagsCount$)
+    let latest = 0;
+    const done$ = new Subject<void>();
+    service.tagsCount$.pipe(
+      takeUntil(done$)
+    ).subscribe(count => {latest = count})
+    for (const tagName of ['a', 'b', 'c']) {
+      await service.StoreTag(tagName)
+    }
+    done$.next()
+    expect(latest).toBe(3);
+  })
+
+  it('stored tags are cached', async () => {
+    const stored = []
+    for (const tagName of ['a', 'b', 'c']) {
+      expect(service.TagByName(tagName)).toBeUndefined()
+      stored.push(await service.StoreTag(tagName))
+    }
+    for (const tag of stored) {
+      expect(service.TagByName(tag.name)).toBeTruthy()
+      expect(service.TagById(tag.reference.id)).toBeTruthy()
+    }
+  })
+
+  it('recent tags are updated based on use', async () => {
+    for (const tagName of ['a', 'b', 'c', 'd', 'e', 'f']) {
+      expect(service.TagByName(tagName)).toBeUndefined()
+      await service.StoreTag(tagName)
+    }
+    const f = service.TagByName('f')
+    const c = service.TagByName('c')
+    expect(f).toBeTruthy()
+    expect(c).toBeTruthy()
+    service.appliedTags$.next([f!, c!])
+    const recent = await firstValueFrom(service.recentTags$)
+    expect(recent).toBeTruthy()
+    expect(recent.map(t=>t.name)).toEqual(['f', 'c', 'a', 'b', 'd', 'e'])
+  })
+
+
+});
---
+++ src/app/tag.service.ts
@@ -0,0 +1,306 @@
+import {inject, Injectable, OnDestroy, Query} from '@angular/core';
+import {
+  Bytes,
+  collection, doc,
+  DocumentReference,
+  DocumentSnapshot,
+  Firestore,
+  getDoc,
+  onSnapshot,
+  query, setDoc
+} from '@angular/fire/firestore';
+import {EncryptionService} from './encryption.service';
+import {MessageService} from './message.service';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {
+  BehaviorSubject,
+  distinctUntilChanged,
+  map,
+  Observable,
+  shareReplay,
+  startWith,
+  withLatestFrom
+} from 'rxjs';
+import {HmacService} from './hmac.service';
+
+export type Tag = {
+  name: string,
+  reference: DocumentReference,
+}
+
+type StoredTag = {
+  name: Bytes;
+  iv?: Bytes,
+  keyReference?: DocumentReference,
+}
+
+type TagMap = Record<string, Tag>
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TagService implements OnDestroy {
+  private encryption = inject(EncryptionService)
+  private firestore = inject(Firestore)
+  private hmac = inject(HmacService)
+  private messageService = inject(MessageService)
+
+  private readonly encoder = new TextEncoder()
+  private readonly decoder = new TextDecoder()
+  private readonly tagsCollectionPath = 'tags'
+  private tagsByName: TagMap = {}
+  private tagsById: TagMap = {}
+
+  // Tags used most recently.  Contains the entire tags$ output, but in order of use.
+  recentTags$: Observable<Tag[]>
+  // Complete set of tags known to TagService.  Eventually catches up to Firestore.
+  tags$ = new BehaviorSubject<Tag[]>([])
+  // Number of tags known to the TagService
+  tagsCount$ = new BehaviorSubject<number>(0)
+  // Tags applied during the last operation.  Supplied externally.  Ideally this is the only point of contact
+  // between TagService and ImageService.
+  appliedTags$ = new BehaviorSubject<Tag[]>([])
+  // Previous emission of recentTags$, which allows continuous updates.
+  private lastRecentlyUsed$ = new BehaviorSubject<Tag[]>([])
+  private unsubTagCollection: any
+
+  constructor() {
+    this.recentTags$ = this.appliedTags$.pipe(
+      takeUntilDestroyed(),
+      withLatestFrom(this.lastRecentlyUsed$.pipe(startWith([])), this.tags$),
+      map(([applied, lastEmission, stored]) => {
+        const appliedIds = applied.map(t=>t.reference.id)
+        let ret: Tag[];
+        if ( stored.length > lastEmission.length ) {
+          ret = stored.filter(t=> !appliedIds.includes(t.reference.id))
+        } else {
+          ret = lastEmission.filter(t=> !appliedIds.includes(t.reference.id))
+        }
+        ret.unshift(...applied)
+        this.lastRecentlyUsed$.next(ret)
+        return ret;
+      }),
+      distinctUntilChanged(),
+      shareReplay(),
+    )
+    this.startSubscriptions()
+  }
+
+  private startSubscriptions(): void {
+    const allTagsQuery = query(collection(this.firestore, this.tagsCollectionPath));
+    this.unsubTagCollection = onSnapshot(allTagsQuery, (querySnapshot) => {
+      const tags: Promise<Tag>[] = [];
+      querySnapshot.forEach((doc) => {
+        tags.push(this.TagFromStorage(doc.data() as StoredTag, doc.ref as DocumentReference));
+      })
+      Promise.all(tags).then(liveTags => {
+        this.tagsByName = Object.fromEntries(liveTags.map(t => [t.name, t]))
+        this.tagsById = Object.fromEntries(liveTags.map(t => [t.reference.id, t]))
+        this.tags$.next(liveTags.sort((a, b) => a.name.localeCompare(b.name)));
+        this.tagsCount$.next(liveTags.length);
+      })
+    })
+  }
+
+  ngOnDestroy() {
+    this.unsubTagCollection();
+  }
+
+  private async TagFromStorage(tag: StoredTag, ref: DocumentReference): Promise<Tag> {
+    if ( !( tag?.iv || tag?.keyReference ) ) {
+      return {
+        'name': this.decoder.decode(tag.name.toUint8Array()),
+        'reference': ref,
+      } as Tag;
+    }
+    if ( !this.encryption.enabled() ) {
+      return Promise.reject(`Loading tag ${ref.id}: tag is encrypted, but EncryptionService is not enabled.`);
+    }
+    const name = await this.encryption.Decrypt(
+      {'ciphertext': tag.name.toUint8Array(), 'iv': tag.iv!.toUint8Array(), keyReference: tag.keyReference!})
+    return {
+      'name': this.decoder.decode(name),
+      reference: ref
+    }
+  }
+
+  private async TagToStorage(tag: Tag): Promise<StoredTag> {
+    const encodedName = this.encoder.encode(tag.name);
+    if ( !this.encryption.enabled() ) {
+      return {'name': Bytes.fromUint8Array(encodedName) } as StoredTag;
+    }
+    const res = await this.encryption.Encrypt(encodedName);
+    return {
+      'name': Bytes.fromUint8Array(new Uint8Array(res.ciphertext)),
+      'iv': Bytes.fromUint8Array(new Uint8Array(res.iv)),
+      'keyReference': res.keyReference,
+    } as StoredTag;
+  }
+
+  /**
+   * Calculates document reference based on HMAC of name.
+   */
+  private async GetTagReference(name: string): Promise<DocumentReference> {
+    const cached = this.TagByName(name)?.reference
+    if ( cached !== undefined ) {
+      return cached;
+    }
+    const id = await this.hmac.getHmacHex(new Blob([name], {type: 'text/plain'}))
+    return doc(this.firestore,  this.tagsCollectionPath, id);
+  }
+
+ /**
+  * Return a cached tag reference based on a Tag's name.  May incur a miss if the tag subscription has not caught up.
+  */
+  TagRefByName(name: string): DocumentReference | undefined {
+    return this.tagsByName[name]?.reference
+  }
+
+ /**
+  * Return cached Tag based on its name.  May incur a miss if the tag subscription has not caught up.
+  * */
+  TagByName(name: string): Tag | undefined {
+    return this.tagsByName[name]
+  }
+
+  /**
+   * Return cached Tag based on its id.  May incur a miss if the tag subscription has not caught up.
+   */
+  TagById(id: string): Tag | undefined {
+    return this.tagsById[id]
+  }
+
+ /**
+  * Loads a Tag based on its document reference (HMAC'd name)
+  */
+  async LoadTagByReference(ref: DocumentReference): Promise<Tag> {
+    const cached = this.TagById(ref.id)
+    if ( cached ) {
+      return cached as Tag;
+    }
+    return new Promise(async (resolve, reject) => {
+      return getDoc(ref)
+        .then((snapshot: DocumentSnapshot) => {
+          if ( snapshot.exists() ) {
+            resolve(this.TagFromStorage(snapshot.data() as StoredTag, ref));
+          } else {
+            const msg = `LoadTagByReference(${ref.id}): not found`
+            this.messageService.Error(msg)
+            reject(msg);
+          }
+        })
+        .catch((err: Error) => {
+          this.messageService.Error(err)
+        })
+    })
+  }
+
+  /**
+   * Create a new Tag with the specified name.  Store it in firestore.
+   */
+  async StoreTag(name: string): Promise<Tag>  {
+    return new Promise(async (resolve, reject) => {
+      const ref = await this.GetTagReference(name);
+      const tag = {name: name, reference: ref} as Tag;
+      const toStorage = await this.TagToStorage(tag)
+      return setDoc(ref, toStorage)
+        .then(()=> {
+          this.messageService.Info(`Created tag: ${name}`)
+          resolve(tag)
+        })
+        .catch((err: Error) => {this.messageService.Error(`Error storing tag ${tag.name}: ${err}`)});
+    });
+  }
+
+  /**
+   * Loads a Tag based on its name.
+   */
+  async LoadTagByName(name: string): Promise<Tag> {
+    const cached = this.TagByName(name);
+    if ( cached !== undefined ) {
+      this.messageService.Info(`Fetched (cached) tag: ${name}`)
+      return cached
+    }
+    const tagRef = await this.GetTagReference(name)
+    return this.LoadTagByReference(tagRef)
+  }
+
+  /**
+   * Remove this tag from all images that reference it and delete the tag document.
+   */
+  async DeleteTag(ref: DocumentReference): Promise<void> {
+    // This will require a transaction based approach where we'll read images and modify all images that
+    // reference the tag.  Should also decrement current tagsCount$
+    throw new Error('Not Implemented')
+  }
+}
+
+
+
+export class FakeTagService {
+
+  // Tags used most recently.  Contains the entire tags$ output, but in order of use.
+  recentTags$: Observable<Tag[]>
+  // Complete set of tags known to TagService.  Eventually catches up to Firestore.
+  tags$ = new BehaviorSubject<Tag[]>([])
+  // Number of tags known to the TagService
+  tagsCount$ = new BehaviorSubject<number>(0)
+  // Tags applied during the last operation.  Supplied externally.
+  appliedTags$ = new BehaviorSubject<Tag[]>([])
+  // Previous emission of recentTags$, which allows continuous updates.
+  private lastRecentlyUsed$ = new BehaviorSubject<Tag[]>([])
+
+  // Fake tags on which all operations will be carried out.
+  private tags: Tag[]
+
+  constructor(tags: Tag[]) {
+    this.tags = tags;
+    this.recentTags$ = this.appliedTags$.pipe(
+      takeUntilDestroyed(),
+      withLatestFrom(this.lastRecentlyUsed$.pipe(startWith([])), this.tags$),
+      map(([applied, lastEmission, stored]) => {
+        const appliedIds = applied.map(t=>t.reference.id)
+        let ret: Tag[];
+        if ( stored.length > lastEmission.length ) {
+          ret = stored.filter(t=> !appliedIds.includes(t.reference.id))
+        } else {
+          ret = lastEmission.filter(t=> !appliedIds.includes(t.reference.id))
+        }
+        ret.unshift(...applied)
+        this.lastRecentlyUsed$.next(ret)
+        return ret;
+      }),
+      distinctUntilChanged(),
+      shareReplay(),
+    )
+  }
+
+  async StoreTag(name: string): Promise<Tag> {
+    const tag = this.tags.filter(t=>t.name === name).pop()
+    if ( !tag) {
+      this.tags.push({name: name, reference: {id: name, path: `tags/${name}`} as DocumentReference})
+    }
+    return Promise.reject(`Exists`);
+  }
+
+  async LoadTagByReference(ref: DocumentReference): Promise<Tag> {
+    const tag = this.tags.filter(t=>t.reference.id === ref.id).pop()
+    if ( !tag) {
+      return Promise.reject('Not found')
+    }
+    return tag as Tag;
+  }
+
+  async LoadTagByName(name: string): Promise<Tag> {
+    const tag = this.tags.filter(t=>t.name === name).pop()
+    if ( !tag) {
+      return Promise.reject('Not found')
+    }
+    return tag as Tag;
+  }
+
+  async DeleteTag(ref: DocumentReference): Promise<void> {
+    this.tags = this.tags.filter(t=> t.reference.id !== ref.id)
+  }
+
+}

fdc3f2153a585ead7d8c5f047d45b2102422b3d5
Eugene Alexeev <eugene.alexeev@gmail.com>
2025-03-15 12:58:18 -0700

    Added support fore file metadata parsing in order to restore tags on re-upload.
    Tested on a 3K image collection, worked well in dev.

--- src/app/upload.service.ts
+++ src/app/upload.service.ts
@@ -3,11 +3,16 @@ import {FileHandle} from './drag-drop.directive';
 import {concatMap, from, Subscription} from 'rxjs';
 import {MessageService} from './message.service';
 import {DocumentReference} from '@angular/fire/firestore';
-import {TagService} from './tag.service';
+import {Tag, TagService} from './tag.service';
 import {ImageService} from './image.service';
 
 interface Metadata {
   tags: string[]
+}
+
+interface FilePair {
+  image: FileHandle;
+  metadata?: FileHandle;
 }
 
 @Injectable({
@@ -31,18 +36,29 @@ export class UploadService implements OnDestroy {
     this.sub.unsubscribe();
   }
 
-  private async receiveImageURL(url: string, tag?: string, metadata?: unknown): Promise<void> {
+  private async processFile(file: File, tag?: string, metadata?: File): Promise<void> {
     const tagNames: Set<string> = new Set();
     if (tag) {
       tagNames.add(tag)
     }
     if (metadata) {
-      (metadata as Metadata).tags.forEach(t => tagNames.add(t))
+      const parsed = JSON.parse(await metadata.text()) as Metadata
+      parsed.tags.forEach(t => tagNames.add(t))
     }
-    const tags = await Promise.all([...tagNames].map(t=> this.tagService.LoadTagByName(t)))
-    const tagRefs = tags.map(t=>t.reference)
-    const blob = await fetch(url).then(res => res.blob()).then(blob => blob)
-    await this.imageService.StoreImage(blob, tagRefs)
+    const tags: DocumentReference[] = [];
+    for (const name of tagNames) {
+      let tag: Tag;
+      try {
+        tag = await this.tagService.LoadTagByName(name);
+        tags.push(tag.reference)
+      } catch (e: unknown) {
+        if ((e as string).includes('not found')) {
+          tag = await this.tagService.StoreTag(name);
+          tags.push(tag.reference);
+        }
+      }
+    }
+    await this.imageService.StoreImage(file, tags)
     this.uploadedCount.update(v=>v+1)
   }
 
@@ -60,8 +76,8 @@ export class UploadService implements OnDestroy {
 
     for (const batch of this.makeBatches(files, queueCount).filter(b => !!b)) {
       this.sub.add(from(batch).pipe(
-        concatMap((file: FileHandle) => {
-          return from(this.receiveImageURL(file.url, tag))
+        concatMap((pair: FilePair) => {
+          return from(this.processFile(pair.image.file, tag, pair.metadata?.file))
         }),
       ).subscribe({
         next: ()=> this.uploadedCount.update(v=>v+1),
@@ -73,13 +89,28 @@ export class UploadService implements OnDestroy {
     }
   }
 
-  private makeBatches(files: FileHandle[], count: number): FileHandle[][] {
-    const batches: FileHandle[][] = [];
+  private makeBatches(files: FileHandle[], count: number): FilePair[][] {
+    const batches: FilePair[][] = [];
     const batchSize = Math.ceil(files.length / count);
+
+    const metadata: Map<string, FileHandle> = new Map();
+
+    for (const fh of files) {
+      if (fh.file.name.endsWith('.json')) {
+        metadata.set(fh.file.name.slice(0, -5), fh);
+      }
+    }
+
+    const images = files.filter(fh => !fh.file.name.endsWith('.json'));
+
     for (let i = 0; i < count; i++) {
       const start = i*batchSize;
       const end = start+batchSize;
-      batches.push(files.slice(start, end));
+      batches.push(images.slice(start, end).map(fh=> (
+        {
+          image: fh,
+          metadata: metadata.get(fh.file.name.slice(0, fh.file.name.lastIndexOf('.')))
+        } as FilePair)));
     }
     return batches;
   }

cb8b7820e5939f82d31c02ea4ac092e3d33a687e
Eugene Alexeev <eugene.alexeev@gmail.com>
2025-03-22 09:13:10 -0700

    Added partial header with buttons to image-view.  Now possible to delete and download an image from the view interface.
    Auto-complete for tags is mocked out, but needs to be wired up to actual image tags and all known tags.

--- src/app/image-view/image-view.component.ts
+++ src/app/image-view/image-view.component.ts
@@ -1,6 +1,28 @@
-import {Component, effect, Input, WritableSignal, signal, OnDestroy, OnInit, inject} from '@angular/core';
+import {
+  Component,
+  effect,
+  Input,
+  WritableSignal,
+  signal,
+  OnDestroy,
+  OnInit,
+  inject,
+  ViewChild,
+  ElementRef, Renderer2
+} from '@angular/core';
 import {Image, ImageData} from '../../lib/models/image.model';
 import {ImageService} from '../image.service';
+import {MatInputModule} from '@angular/material/input';
+import {MatChipInputEvent, MatChipsModule} from '@angular/material/chips';
+import {MatAutocompleteModule, MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {map, Observable, startWith} from 'rxjs';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {MatIconModule} from '@angular/material/icon';
+import {AsyncPipe, Location} from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatDividerModule} from '@angular/material/divider';
 
 export interface ImageBundle {
   image: Image
@@ -11,10 +33,23 @@ export interface ImageBundle {
   selector: 'app-image-view',
   standalone: true,
   templateUrl: './image-view.component.html',
+  imports: [
+    MatDividerModule,
+    MatInputModule,
+    MatChipsModule,
+    MatAutocompleteModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+    MatButtonModule,
+    MatTooltipModule
+  ],
   styleUrl: './image-view.component.scss'
 })
 export class ImageViewComponent implements OnInit, OnDestroy {
 
+  private renderer: Renderer2 = inject(Renderer2);
+  private location: Location = inject(Location);
   private imageService: ImageService = inject(ImageService);
 
   // This is set if we get to a page via a URL, it is static and not sensitive to changes.
@@ -27,6 +62,14 @@ export class ImageViewComponent implements OnInit, OnDestroy {
 
   private bundle: WritableSignal<ImageBundle|null> = signal(null);
   protected imgURL: WritableSignal<string> = signal('');
+  protected imgExtension: WritableSignal<string> = signal('');
+
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  itemCtrl = new FormControl('');
+  filteredItems: Observable<string[]>;
+  items: string[] = ['Apple', 'Banana', 'Orange', 'Grapes'];
+  selectedItems: string[] = ['Apple'];
+  @ViewChild('itemInput') itemInput!: ElementRef;
 
   constructor() {
     effect( async () => {
@@ -36,7 +79,15 @@ export class ImageViewComponent implements OnInit, OnDestroy {
       }
       const b = await newBundle.data.fullSize()
       this.imgURL.set(URL.createObjectURL(b));
-    })
+      this.imgExtension.set(extFromMime(b.type));
+    });
+
+    this.filteredItems = this.itemCtrl.valueChanges.pipe(
+      startWith(null),
+      map((item: string | null) =>
+        item ? this._filter(item) : this.items.slice()
+      )
+    );
   }
 
   async ngOnInit(): Promise<void> {
@@ -62,4 +113,58 @@ export class ImageViewComponent implements OnInit, OnDestroy {
     }
   }
 
+  addTag(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    if (value) {
+      this.selectedItems.push(value);
+    }
+
+    event.chipInput!.clear();
+    this.itemCtrl.setValue(null);
+  }
+
+  removeTag(item: string): void {
+    const index = this.selectedItems.indexOf(item);
+
+    if (index >= 0) {
+      this.selectedItems.splice(index, 1);
+    }
+  }
+
+  select(event: MatAutocompleteSelectedEvent): void {
+    this.selectedItems.push(event.option.viewValue);
+    this.itemInput.nativeElement.value = '';
+    this.itemCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.items.filter((item) =>
+      item.toLowerCase().includes(filterValue)
+    );
+  }
+
+  protected async onDelete() {
+    const bundle = this.bundle()
+    if (!bundle) {return}
+    await this.imageService.DeleteImage(bundle.image.reference)
+    this.location.back();
+  }
+
+  protected async onDownload() {
+    const bundle = this.bundle();
+    if (!bundle) {return}
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', this.imgURL());
+    link.setAttribute('download', `${bundle.image.reference.id}.${this.imgExtension()}`);
+    link.click();
+    link.remove();
+  }
+
+}
+
+function extFromMime(mimeType: string): string {
+  return mimeType.slice('image/'.length)
 }

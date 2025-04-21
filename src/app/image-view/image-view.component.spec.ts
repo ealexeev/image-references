f30c74702c0d60c51446003d43de6570eb9c6dbe
Eugene Alexeev <eugene.alexeev@gmail.com>
2025-04-20 18:46:37 -0700

    Fixed all tests!

--- src/app/image-view/image-view.component.spec.ts
+++ src/app/image-view/image-view.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ImageViewComponent } from './image-view.component';
+import {DefaultProviders} from '../test-providers';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('ImageViewComponent', () => {
+  let providers: DefaultProviders;
   let component: ImageViewComponent;
   let fixture: ComponentFixture<ImageViewComponent>;
 
   beforeEach(async () => {
+    providers = new DefaultProviders();
     await TestBed.configureTestingModule({
-      imports: [ImageViewComponent]
+      providers: providers.getProviders(),
+      imports: [ImageViewComponent, NoopAnimationsModule]
     })
     .compileComponents();
 

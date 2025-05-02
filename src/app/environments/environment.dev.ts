816ea7f66026616f95b34b7c9f1568c0c78e0144
Eugene Alexeev <eugene.alexeev@gmail.com>
2025-05-02 08:25:30 -0700

    Added a visual cue when a duplicate image is uploaded.

--- src/app/environments/environment.dev.ts
+++ src/app/environments/environment.dev.ts
@@ -1,14 +1,31 @@
+// export const environment = {
+// 	"projectId": "demo-project",
+// 	"appId": "***REDACTED APP ID***",
+// 	"storageBucket": "***REDACTED***.firebasestorage.app",
+// 	"apiKey": "***REDACTED API KEY***",
+// 	"authDomain": "***REDACTED***.firebaseapp.com",
+// 	"messagingSenderId": "***REDACTED SENDER ID***",
+//   "firestoreUseLocal": true,
+//   "firebaseStorageUseLocal": true,
+//   "firebaseAuthUseLocal": true,
+//   "authRequired": true,
+//   "authorizedUids": [
+//     "***REDACTED UID***",
+//   ],
+// };
+
 export const environment = {
-	"projectId": "demo-project",
+	"projectId": "***REDACTED***",
 	"appId": "***REDACTED APP ID***",
 	"storageBucket": "***REDACTED***.firebasestorage.app",
 	"apiKey": "***REDACTED API KEY***",
 	"authDomain": "***REDACTED***.firebaseapp.com",
 	"messagingSenderId": "***REDACTED SENDER ID***",
-  "firestoreUseLocal": true,
-  "firebaseStorageUseLocal": true,
-  "firebaseAuthUseLocal": true,
+  "firestoreUseLocal": false,
+  "firebaseStorageUseLocal": false,
+  "firebaseAuthUseLocal": false,
   "authRequired": true,
+  "databaseId": "test-references",
   "authorizedUids": [
     "***REDACTED UID***",
   ],
